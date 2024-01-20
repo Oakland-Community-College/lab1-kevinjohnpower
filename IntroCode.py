5acbe99eb823a5a0d7086b636a62f1d7748ff15a
kevinjohnpower <131912894+kevinjohnpower@users.noreply.github.com>
2024-01-20 10:58:48 -0500

    Add files via upload

---
+++ IntroCode.py
@@ -0,0 +1,5 @@
+print("Hi, I am Becca Bates, a high school math teacher on the eve of retirement.")
+print("I started taking some IT classes at OCC for the fun of it and maybe to transition into a\n"
+      " new career if I get bored out of the classroom.")
+print("The last time I took a programming class was 1985 and I enjoyed it then, so I am hoping to enjoy this as well.")
+print("If anybody is willing to partner with me, I promise not to make you explain technology to me.")
